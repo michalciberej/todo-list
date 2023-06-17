527985ac432e85da1cf96c6a1cf8b4965f2c3d8c
Michal Ciberej <michal.ciberej@gmail.com>
2023-06-17 17:21:50 +0200

    Finish layout of the page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import "./style.css"
 
+document.querySelector("#burgerMenu").addEventListener("click", () => displayProjectsMenu())
 
-console.log("retard")
+function displayProjectsMenu() {
+    const pO = document.querySelectorAll(".projectsOptions")
+    const pC = document.querySelector("#projectsContainer")
+    const mC = document.querySelector("#mainContainer")
+    if (pC.style.width === "15vw") {
+        pC.style.width = "0vw"
+        mC.style.marginLeft = "0vw"
+        displayProjectOptions(pO, 0)
+    }   else if (pC.style.width === "" || pC.style.width === "0vw") {
+        pC.style.width = "15vw"
+        mC.style.marginLeft = "15vw"
+        displayProjectOptions(pO, 1)
+    }
+}
+
+function displayProjectOptions(arr, size) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].style.scale = size
+    }
+}
